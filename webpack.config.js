77a3af36ae8169a8524be322b7b4666b1e6979d3
Chisom Onwuchekwa <princechekwas@gmail.com>
2019-06-22 22:46:34 +0100

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+require('dotenv').config();
+
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: './public/index.html',
+  filename: './index.html'
+});
+
+const cssPlugin = new MiniCssExtractPlugin({
+  filename: './assets/css/style.css'
+});
+
+module.exports = {
+  entry: './src/App.jsx',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.css']
+  },
+  node: {
+    net: 'empty',
+    fs: 'empty'
+  },
+  stats: {
+    colors: true,
+    reasons: true
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.jsx?$/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.(scss|css|sass)$/,
+        use: [
+          'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../../'
+            }
+          },
+          'css-loader',
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: '[name].[ext]',
+              outputPath: './assets/img/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: ['html-loader']
+      }
+    ]
+  },
+  plugins: [
+    cssPlugin,
+    htmlPlugin,
+  ]
+};
