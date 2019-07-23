830984a54e0b3d924887313d54a17601cb7cf439
Chisom Onwuchekwa <princechekwas@gmail.com>
2019-07-23 11:07:23 +0100

    add server file

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, './dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './dist/index.html'));
+});
+
+const port = process.env.PORT || Number(3080);
+
+app.listen(port);
+
+export default app;
